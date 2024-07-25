e61c0177f51def091d3e3218986dbdd431890b25
INIT-REF <rufmichan@freenet.de>
2024-07-25 10:20:41 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Mastermind_C
-A Mastermind game in C
+A simple CLI based Mastermind game in C.
+
+## Usage
+If you choose Classic mode you can place 6 digits (1-6 representing the 6 available colors of the original game) in 4 spots, and you have 12 guesses to find the correct sequence. In "Super" mode you have 8 digits (1-8), 5 spots and still 12 guesses.
+
+You can also chose to create a game with custom options, chosing between 2 to 10 digits (1-9 and 0 in the latter case), 2 to 10 spots and an arbitrary number of guesses (0 for unlimited).
+
+## Rules
+You probably already know the rules of the game, but I'll recap them quickly:
+
+The game will generate a random digit sequence based on the chosen game mode, without showing it to you. 
+
+E.g. in Classic mode it could be 3461, 3335, 1662 etc.
+In "Super" mode it could be 83652, 11122, 54251 etc.
+In Custom mode with 3 digits and 10 spots it could be 1221331232, 1112322231, 3322111232 etc.
+
+Your task is to find this sequence using as few guesses as possible, at least within the limit given by the game mode or your choice in custom mode.
+
+When you entered a guess, the game will evaluate the input and output a hint as a sequence of black and/or white dot (🌕 and/or 🌑), giving you information on how good your guess was. A black dot means that you have a correct digit at the correct position. A white dot means that you have a digit in your guess which is also present in the sequence, but it is not at the correct spot (not counting digits which were already marked with a black dot). The position of the dots is independent of the position of the digits in your guess. Black dots will always appear first, followed by white dots.
+
+E.g. if the hidden sequence is 1234:
+
+guess | hint     | comment (not shown in the game)
+------+----------+-------------------------------------------------------------
+1561  | 🌕       | a black dot for the first 1
+4321  | 🌑🌑🌑🌑 | all digits are there, but none in the correct spot
+2154  | 🌕🌑🌑   | 1 and 2 are correct but in the wrong spot, a black dot for 4
+
+## To do's
+- Let the user chose a time limit instead (or in addition to) a number of guesses
+- Show the number of guesses and/or time needed to get the solution
+- Allow the user to edit a guess before submission
+- General cleanup
--- mastermind.c
+++ mastermind.c
@@ -2,18 +2,24 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <termios.h>
+#include <time.h>
+#include <string.h>
 
-#define VERSION 0.1
+#define VERSION "0.1"
+#define MAXOPTIONS 4
+
 
 struct termios saved_attributes;
 
+
 // Reset terminal to the state it had before
-void reset_input_mode (void) {
+void reset_input_mode(void) {
     tcsetattr (STDIN_FILENO, TCSANOW, &saved_attributes);
 }
 
+
 // Set terminal to raw mode
-void set_raw_mode (void) {
+void set_raw_mode(void) {
     struct termios tattr;
     char *name;
 
@@ -32,31 +38,126 @@ void set_raw_mode (void) {
     tcsetattr (STDIN_FILENO, TCSAFLUSH, &tattr);
 }
 
-void welcome(void) {
-    printf("\nWelcome to Mastermind_C version VERSION\n");
-}
-
-void menu(void) {
-}
 
 // Evaluate useres guess
-void evaluate(char* set, char* guess) {
-    printf("🌑🌑🌑🌕🌕🌕\n");
+void evaluate(char* set, int d, int s, int g) {
+    char* guess = (char*)malloc(s * sizeof(char));
+    char* found = (char*)malloc(s * sizeof(char));
+    int correct = 0, semi;
+
+    for (int i = 0; i < s; i++)
+        printf("%d", set[i]);
+    printf("\n\n");
+
+    while (correct < s && g--) {
+        correct = 0;
+        semi = 0;
+        memset(found, 0, sizeof(found));
+
+        for (int i = 0; i < s; i++) {
+            read(STDIN_FILENO, &guess[i], 1);
+            guess[i] -= '0';
+            if (guess[i] == set[i]) {
+                correct++;
+                found[i] = 1;
+            }
+        }
+
+        for (int i = 0; i < s; i++)
+            for (int j = 0; j < s; j++) 
+                if (guess[i] == set[j] && !found[i])
+                    semi++;
+
+        printf(" | ");
+
+        for (int i = 0; i < correct; i++)
+            printf("🌕");
+        for (int i = 0; i < semi; i++)
+            printf("🌑");
+
+        printf("\n");
+    }
+    
+    if (correct == s)
+        printf("Congratulations!\n");
+    else
+        printf("Sorry\n");
 }
 
-int main(int argc, char **argv) {
-    char c;
 
-    set_raw_mode ();
+// Start a new game
+void new_game(int d, int s, int g) {
+    char* set = (char*)malloc(s * sizeof(char));
 
-    welcome();
-    menu();
+    if (g == 0)
+        printf("New game with %d digits, %d spots and unlimited guesses\n", d, s);
+    else
+        printf("New game with %d digits, %d spots and %d guesses\n", d, s, g);
 
-    while (1) {
-        read (STDIN_FILENO, &c, 1);
-        if (c == '\004')          /* C-d */
-            break;
+    for (int i = 0; i < s; i++) {
+        set[i] = (1 + rand() % d) % 10;
     }
 
-  return EXIT_SUCCESS;
+    printf("Please enter your first guess:\n\n");
+    set_raw_mode();
+    evaluate(set, d, s, g);
+}
+
+
+// Get parameters for a new game with custom settings
+void custom_game(void) {
+    int d, s, g;
+
+    printf("\nPlease enter the number of possible digits (2-10): ");
+    scanf("%d", &d);
+    while (d < 2 || d > 10) {
+        printf("Invalid number of digits, please enter a number from 2 to 10: ");
+        scanf("%d", &d);
+    }
+
+    printf("\nPlease enter the number of spots (2-10): ");
+    scanf("%d", &s);
+    while (s < 2 || s > 10) {
+        printf("Invalid number of spots, please enter a number from 2 to 10: ");
+        scanf("%d", &s);
+    }
+
+    printf("\nPlease enter the number of guesses (0 for unlimited): ");
+    scanf("%d", &g);
+    while (g < 0) {
+        printf("Invalid number of guesses, please enter a positive number (or 0 for unlimited): ");
+        scanf("%d", &g);
+    }
+
+    new_game(d, s, g);
+}
+
+
+// Display the menu and get user's choice
+void menu(void) {
+    printf("+------------------------------------------------------------------------+\n");
+    printf("|         Main menu - Enter your choice as a number from 1 to %d          |\n", MAXOPTIONS);
+    printf("+------------------------------------------------------------------------+\n");
+    printf("| 1 - Start a new game in Classic mode (6 digits, 4 spots, 12 guesses)   |\n");
+    printf("| 2 - Start a new game in \"Super\" mode (8 digits, 5 spots, 12 guesses)   |\n");
+    printf("| 3 - Start a new game with custom settings                              |\n");
+    printf("| 4 - Quit (You can exit anytime by pressing Ctrl+C)                     |\n");
+    printf("+------------------------------------------------------------------------+\n");
+    
+    switch (getchar()) {
+        case '1': new_game(6, 4, 12); break;
+        case '2': new_game(8, 5, 12); break;
+        case '3': custom_game(); break;
+        case '4': exit(0); break;
+        default: printf("Invalid option, please enter a number from 1 to %d\n", MAXOPTIONS);
+    }
+}
+
+
+int main(int argc, char **argv) {
+    srand(time(NULL));
+    printf("\nWelcome to Mastermind_C version %s\n\n", VERSION);
+    menu();
+
+    return EXIT_SUCCESS;
 }
